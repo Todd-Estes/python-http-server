932f72b7adaa7cf27891b5bb54a9fff83611447f
todd-estes <elestes@gmail.com>
2024-08-23 17:04:14 -0400

    Add /user-agent endpoint functionality

--- app/main.py
+++ app/main.py
@@ -20,17 +20,18 @@ def main():
     # create request headers hash
     request_headers = {}
     while True:
-      print("in header loop")
       line = request_file.readline().decode('utf-8').strip()
       if not line: break
       header_key, header_value = line.split(': ')
       request_headers[header_key] = header_value
 
-
     if request_target == "/":
       client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n") # .encode() at end of string acts same as 'b' prefix
+    elif request_target == "/user_agent":
+      response_body = request_headers["User-Agent"]
+      content_length = len(return_string.encode())
+      response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{response_body}".encode()
     elif request_target.split("/")[1:][0] == "echo":
-
       return_string = request_target.split("/")[1:][1]
       content_length = len(return_string.encode())
       response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\n{return_string}".encode()
