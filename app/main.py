c19d23e6685504517a6311ad90ff2af93d2a1f00
todd-estes <elestes@gmail.com>
2024-08-24 20:56:12 -0400

    Add thread object; single connection works

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import socket
 import io
+import threading
 
-def main():
+def run_server():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -41,4 +42,6 @@ def main():
       client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 if __name__ == "__main__":
-    main()
+    server_thread = threading.Thread(target=run_server())
+    server_thread.start()
+    server_thread.join()
